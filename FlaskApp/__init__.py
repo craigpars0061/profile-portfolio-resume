7157139964eb4d029deb832d19ad43ba081a9441
Craig Parsons <CraigPars0061@gmail.com>
2016-06-11 23:06:33 -0700

    Header Footer
    Trying out a simple header footer setup

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -2,9 +2,17 @@
 
 from flask import Flask, render_template
 app = Flask(__name__)
+
+# Base route.
 @app.route("/")
 def startMain():
     return render_template("main.html")
+   
+# This seems to be the simple routing system.
+
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run()
--- FlaskApp/__main__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from Flask import Flask
-
-app = Flask(__name__)
-from app import views
