eee7fd0466549c9f78fa1bf6b781a9e86188153a
Craig Parsons <CraigPars0061@gmail.com>
2016-07-05 20:25:47 -0700

    Moving the Tawk script call to a init file.

--- FlaskApp/static/js/init.js
+++ FlaskApp/static/js/init.js
@@ -1,8 +1,21 @@
-(function($){
-  $(function(){
+var $_Tawk_API = {},
+    $_Tawk_LoadStart = new Date();
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+(function($) {
+    $(function() {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
 
-  }); // end of document ready
+        // Start of Tawk.to Script
+        var scriptElement = document.createElement("script"),
+            scriptTag = document.getElementsByTagName("script")[0];
+
+        scriptElement.async = true;
+        scriptElement.src = 'https://embed.tawk.to/577c2baae1416e7e7c59cbf4/default';
+        scriptElement.charset = 'UTF-8';
+        scriptElement.setAttribute('crossorigin', '*');
+        scriptTag.parentNode.insertBefore(scriptElement, scriptTag);
+        // Edn of Tawk.to script.
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
